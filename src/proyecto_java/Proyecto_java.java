1c0eb103015246b1e25766a1097a9359690eddac
Giovanni-Lopez <alexisange001@gmail.com>
2021-05-07 17:27:44 -0600

    Se añadio los metodos get y set. tambien se creo el objeto de la clase. etcetera

--- src/proyecto_java/Proyecto_java.java
+++ src/proyecto_java/Proyecto_java.java
@@ -8,13 +8,58 @@ import java.util.Scanner;
  */
 public class Proyecto_java {
 
-    public String nombre;
     public String apellidos;
     public int edad;
     public String estudio;
     
-    public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);                
+    public String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEstudio() {
+        return estudio;
+    }
+
+    public void setEstudio(String estudio) {
+        this.estudio = estudio;
     }
     
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);       
+        Proyecto_java obj = new Proyecto_java();
+        
+        System.out.println("Nombre: ");
+        obj.setNombre(leer.nextLine());
+        
+        System.out.println("Apellidos: ");
+        obj.setApellidos(leer.nextLine());
+        
+        System.out.println("Edad: ");
+        obj.setEdad(leer.nextInt());
+        
+        System.out.println("Estudio: ");
+        obj.setEstudio(leer.nextLine());        
+    }    
 }
